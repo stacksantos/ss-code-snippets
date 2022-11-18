a807d386238a74eebd7fa8c5a26968981ed9bafc
C. L. Santos <stacksantos@proton.me>
2022-11-18 15:20:22 -0500

    fix arrow direction

--- amend-commit-date.js
+++ amend-commit-date.js
@@ -1,7 +1,7 @@
 -git status // to check your staging status
 -git add . // add all files
 // This next part is all on one line
--git commit -m "[Add your message here]" --amend --date="Aug 24 20:00:00 2022" Put the date you want in here ^^^
+-git commit -m "[Add your message here]" --amend --date="Aug 24 20:00:00 2022" <-- Put the date you want in here
 //
 -git pull ** to update your local branch **
 -git push
